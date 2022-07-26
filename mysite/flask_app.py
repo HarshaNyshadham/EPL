59088b70e100f108ef937e84856d0a97b505ec35
Harshanand Nyshadham <harsha.nyshadham@gmail.com>
2022-07-26 10:27:11 -0500

    a

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -6,6 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask! Test passed'
+@app.route('/index')
+def index():
+  return render_template('index.html')
 
